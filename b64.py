108c74d0966544364db0f17435d3e06927318eaa
J <6201093+Orphan-Crippler@users.noreply.github.com>
2021-10-19 23:54:22 -0400

    initial creation of app to a working state

---
+++ b64.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from base64 import *
+
+def app(st):
+
+    if st == '1':
+        print('Enter M to return to main menu\n')
+        code = input('Enter Stuff to Encode: ').encode()
+        if code.decode() == 'M':
+            return
+        print('\n')
+        try:
+            ans = b64encode(code)
+            print('\n',str(ans)[2:-1],'\n')
+            return
+        except Exception as x:
+            print('!!!!!!!!!!!!!!!!!!!!!!!! WARNING: ',x,'\n')
+    elif st == '2':
+        print('Enter M to return to main menu\n')
+        code = input('Enter Stuff to Decode: ').encode()
+        if code.decode() == 'M':
+            return
+        print('\n')
+        try:
+            ans = b64decode(code)
+            print('\n',str(ans)[2:-1],'\n')
+            return
+        except Exception as x:
+            print('!!!!!!!!!!!!!!!!!!!!!!!! WARNING: ',x,'\n')
+    elif st == 'q':
+        exit()
+    else:
+        print('\nPlease Enter a Valid Entry!\n')
+        return
+
+while True:
+    app(st = input('\nEnter 1 to Encode\n\nEnter 2 to Decode\n\nEnter q to Quit\n'))
