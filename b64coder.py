967dea039cd65fa176349d0149c66992abb7a586
Orphan-Crippler <6201093+Orphan-Crippler@users.noreply.github.com>
2023-02-25 22:19:48 -0500

    finished file menu bar functionality

--- b64coder.py
+++ b64coder.py
@@ -1,7 +1,59 @@
 from tkinter import *
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 from base64 import b64encode, b64decode
 import os
+
+fileName = ""
+
+# File Functions
+def nFile():
+    global fileName
+    msg_save = messagebox.askquestion('Save Document?', 'Would you like to Save the current document?', icon='warning')
+    
+    if msg_save == 'yes':
+        saveAs()
+        clear()
+    else:
+        fileName = ""
+        clear()
+
+def oFile():
+    global fileName
+    
+    if text != "":
+        clear()
+
+    f = filedialog.askopenfile(mode='r')
+    fileName = f.name
+    fn = open(f.name, 'r').read()
+    text.insert(1.0, fn)
+    f.close()
+
+def sFile():
+    global fileName
+    
+    if fileName == "":
+        saveAs()
+    else:
+        f = open(fileName, "w")
+        txtSav = str(text.get(1.0, END))
+        f.write(txtSav)
+        f.close() 
+
+def sFileAs():
+    global fileName
+    f = filedialog.asksaveasfile(mode="w", defaultextension="")
+    
+    if f is None:
+        return
+    
+    fileName = f.name
+    txtSav = str(text.get(1.0, END))
+    f.write(txtSav)
+    f.close()
+
+def quit():
+    exit()
 
 # Encode and Decode Functions
 def encode():
@@ -14,7 +66,6 @@ def encode():
     except Exception as x:
         messagebox.showerror(title="ERROR!", message=x)
 
-
 def decode():
     dec = text.get(1.0, "end")
     try:
@@ -25,31 +76,18 @@ def decode():
     except Exception as x:
         messagebox.showerror(title="ERROR!", message=x)
 
-
 # Textbox context commands
 def clear():
     text.delete(1.0, "end")
 
-
 def cut():
     text.event_generate("<<Cut>>")
-
 
 def copy():
     text.event_generate("<<Copy>>")
 
-
 def paste():
     text.event_generate("<<Paste>>")
-
-
-def about():
-    messagebox.showinfo(message="Base 64 Encoder/Decoder\nCreated By: J.Low")
-
-
-def quit():
-    exit()
-
 
 # Setting up Main Form
 root = Tk()
@@ -64,39 +102,31 @@ root.resizable(FALSE, FALSE)
 
 # Working on implementing Filebar
 m = Menu(root)
-f_edit = Menu(m, tearoff=0)
-m.add_cascade(menu=f_edit, label="File")
-f_edit.add_command(label="New", underline=1)
-f_edit.add_command(label="Open", underline=1)
-f_edit.add_command(label="Save", underline=1)
-f_edit.add_command(label="Save As")
-f_edit.add_separator()
-f_edit.add_command(label="Close", underline=1)
-f_edit.add_separator()
-f_edit.add_command(label="Exit", underline=2, command=quit)
-m_edit = Menu(m, tearoff=0)
-m.add_cascade(menu=m_edit, label="Edit")
-m_edit.add_command(
-    label="Cut", command=lambda: root.focus_get().event_generate("<<Cut>>")
-)
-m_edit.add_command(
-    label="Copy", command=lambda: root.focus_get().event_generate("<<Copy>>")
-)
-m_edit.add_command(
-    label="Paste", command=lambda: root.focus_get().event_generate("<<Paste>>")
-)
-m_edit.add_separator()
-m_edit.add_command(
-    label="Find", command=lambda: root.event_generate("<<OpenFindDialog>>")
-)
 root["menu"] = m
 
+f_edit = Menu(m, tearoff=0)
+m.add_cascade(menu=f_edit, label="File")
+f_edit.add_command(label="New", underline=1, command=nFile)
+f_edit.add_command(label="Open", underline=1, command=oFile)
+f_edit.add_command(label="Save", underline=1, command=sFile)
+f_edit.add_command(label="Save As", command=sFileAs)
+f_edit.add_separator()
+f_edit.add_command(label="Exit", underline=2, command=quit)
 
-def launchFindDialog(*args):
-    messagebox.showinfo(message="I hope you find what you are looking for!")
+m_edit = Menu(m, tearoff=0)
+m.add_cascade(menu=m_edit, label="Edit")
+m_edit.add_command(label="Cut", command=lambda: root.focus_get().event_generate("<<Cut>>"))
+m_edit.add_command(label="Copy", command=lambda: root.focus_get().event_generate("<<Copy>>"))
+m_edit.add_command(label="Paste", command=lambda: root.focus_get().event_generate("<<Paste>>"))
+m_edit.add_separator()
+m_edit.add_command(label="Find")
+m_edit.add_separator()
+m_edit.add_command(label="Clear", underline=1, command=clear)
 
-
-root.bind("<<OpenFindDialog>>", launchFindDialog)
+e_edit = Menu(m, tearoff=0)
+m.add_cascade(menu=e_edit, label="Code")
+e_edit.add_command(label="Encode", command=encode)
+e_edit.add_command(label="Decode", command=decode)
 
 # Setting up Frames
 mainframe = ttk.Frame(root, padding="3 3 3 3")
@@ -127,12 +157,6 @@ ys = ttk.Scrollbar(mainframe, orient="vertical", command=text.yview)
 text["yscrollcommand"] = ys.set
 ys.grid(column=1, row=0, sticky="ns", pady=5)
 text.grid(column=0, row=0, sticky="N", pady=5)
-encbutt = ttk.Button(twoframe, text="Encode", command=encode)
-decbutt = ttk.Button(twoframe, text="Decode", command=decode)
-clearbutt = ttk.Button(twoframe, text="Clear", command=clear)
-encbutt.grid(column=1, row=0, sticky="N", pady=5)
-decbutt.grid(column=1, row=1, sticky="N", pady=10)
-clearbutt.grid(column=1, row=2, sticky="S", pady=10)
 
 # Insert instructions into textbox and bring it into focus when starting up
 text.insert(
